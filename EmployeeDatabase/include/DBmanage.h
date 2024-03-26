72cded193db3a6a8a807785a55248c853cfffe1a
Darshan Chandegara <darshan06ppsv@gmail.com>
2024-03-26 19:04:14 +0530

    Add user define table functionality

--- EmployeeDatabase/include/DBmanage.h
+++ EmployeeDatabase/include/DBmanage.h
@@ -16,32 +16,32 @@ using namespace utility;
 
 namespace DB {
 	class Database {
-		char* errorMsg = 0;
 
 		Database() = default;
 		~Database() {
-			//sqlite3_close(db);
 			close();
 		}
 		Database(const Database&) = delete;
 		Database& operator=(const Database&) = delete;
 
 	public:
+		char* errorMsg = 0;
 		sqlite3* db{};
 		int rc{ 0 };
 		sqlite3_stmt* stmt{};
+
 		static int row;
+
 		bool open(const char* str);
 		bool close();
 		int executeQuery(const char* sql, float count = 0);
 		bool selectQuery(const char* sql);
+
 		static Database& getInstance() {
 			static Database db;
 			return db;
 		}
-		void createTableQuery();
-		void deleteTableQuery();
-		void showTables();
+
 		void writeCSV();
 		bool exportToCsv(const std::string_view&, const std::filesystem::path&);
 		static int callback(void* data, int args, char** row, char** col);
--- EmployeeDatabase/main.cpp
+++ EmployeeDatabase/main.cpp
@@ -4,16 +4,29 @@
 #include "include/Helper.h"
 #include "include/LOG/log.h"
 #include "include/LOG/logger.h"
+#include "include/Model/Table.h"
 
 using namespace utility;
 
 void menu();
 
 int main() {
-	logging::default_logger()->setFileFlag(true); 
+	//logging::default_logger()->setFileFlag(true); 
 	DB::Database::getInstance().open("employee.db");
 	auto ch{ true };
-	while (ch) {
+	//Model::Table t;
+	std::optional< Model::Table> t = Model::Table::getTable("demo"); 
+	if (t.has_value()) { 
+		//t.value().infoTable();  
+		//t.value().insertRecord(); 
+		//t.value().deleteRecord();  
+		//t.value().viewAllRecords();
+		t.value().viewRecord();
+	}
+
+	
+	
+	/*while (ch) {
 		system("cls");
 		std::cout << "------------------------------------> WELCOME TO EMPLOYEE MANAGEMENT SYSTEM <-------------------------------------------\n";
 		std::cout << "Enter the Opertaion you want to perform\n";
@@ -28,15 +41,15 @@ int main() {
 		i = std::stoi(input("Enter Choice: " , std::regex{"[1-6]"}));
 		switch (i) {
 		case 1 :
-			DB::Database::getInstance().createTableQuery();
+			t.createTable();
 			break;
 
 		case 2:
-			DB::Database::getInstance().showTables();
+			t.showTables();
 			break;
 
 		case 3:
-			DB::Database::getInstance().deleteTableQuery();
+			t.deleteTable();
 			break;
 
 		case 4:
@@ -54,5 +67,5 @@ int main() {
 		default:
 			std::cout << "Enter valid input: \n";
 		}
-	}
+	}*/
 }
